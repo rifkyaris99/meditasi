0ef5b6821a8fd670624e0f9243029b16fda320c7
rifkyaris99 <rifky.aris@gmail.com>
2024-12-15 06:48:43 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  title: "Coloring Me",
-  tagline: "Coloring Me Free Coloring Page",
-  description: "Coloring Me Free Coloring Pag.",
-  keywords: "coloring,coloringkids,agc,cdom blog",
-  author: "Coloringme.biz.id",
-  favicon: "https://pugjs.org/images/favicon.ico",
+  title: "Raih Kedamaian dengan Meditasi: Cara Mudah untuk Relaksasi",
+  tagline: "lifeignite",
+  description: "Temukan cara mudah untuk bermeditasi dan nikmati manfaatnya bagi tubuh dan pikiran. Meditasi membantu meredakan stres dan meningkatkan kebahagiaan.",
+  keywords: "meditasi, cara meditasi, meditasi yang benar, meditasi agama",
+  author: "lifeignite.web.id",
+  favicon: "https://cdn2.iconfinder.com/data/icons/colored-weather/96/sun_sunny_summer_hot_forecast_weather-1024.png",
   imageCount: 20,
-  removeBadWords: "porn,nude,sex",
+  removeBadWords: "porn,nude,sex,togel,slot,toto,bet,judi",
 };
